f805ea6ceae506df25d6f0cde901359702d521b0
xaz89 <dcg.jip@hotmail.com>
2020-04-30 19:09:01 -0500

    ajustes en todos los menus de los colores

--- js/main.js
+++ js/main.js
@@ -1,47 +1,40 @@
+//////////========== Para iniciar tooltipped ============//////////
+var elems = document.querySelectorAll('.tooltipped');
+var instances = M.Tooltip.init(elems, {position: 'right'});
+
+
 //////////========== Para sidenav icon ============//////////
 
 (function($) {
-    "use strict"; // Start of use strict
-    // Configure tooltips for collapsed side navigation
-    $('.navbar-sidenav [data-toggle="tooltip"]').tooltip({
-      template: '<div class="tooltip navbar-sidenav-tooltip" role="tooltip" style="pointer-events: none;"><div class="arrow"></div><div class="tooltip-inner"></div></div>'
-    })
-    // Toggle the side navigation
-    $("#sidenavToggler").click(function(e) {
-      e.preventDefault();
-      $("body").toggleClass("sidenav-toggled");
-      $(".navbar-sidenav .nav-link-collapse").addClass("collapsed");
-      $(".navbar-sidenav .sidenav-second-level, .navbar-sidenav .sidenav-third-level").removeClass("show");
-    });
-    // Force the toggled class to be removed when a collapsible nav link is clicked
-    $(".navbar-sidenav .nav-link-collapse").click(function(e) {
-      e.preventDefault();
-      $("body").removeClass("sidenav-toggled");
-    });
-    // Prevent the content wrapper from scrolling when the fixed side navigation hovered over
-    $('body.fixed-nav .navbar-sidenav, body.fixed-nav .sidenav-toggler, body.fixed-nav .navbar-collapse').on('mousewheel DOMMouseScroll', function(e) {
-      var e0 = e.originalEvent,
-        delta = e0.wheelDelta || -e0.detail;
-      this.scrollTop += (delta < 0 ? 1 : -1) * 30;
-      e.preventDefault();
-    });
-    // Scroll to top button appear
-    $(document).scroll(function() {
-      var scrollDistance = $(this).scrollTop();
-      if (scrollDistance > 100) {
-        $('.scroll-to-top').fadeIn();
-      } else {
-        $('.scroll-to-top').fadeOut();
-      }
-    });
-    // Configure tooltips globally
-    $('[data-toggle="tooltip"]').tooltip()
-    // Smooth scrolling using jQuery easing
-    $(document).on('click', 'a.scroll-to-top', function(event) {
-      var $anchor = $(this);
-      $('html, body').stop().animate({
-        scrollTop: ($($anchor.attr('href')).offset().top)
-      }, 1000, 'easeInOutExpo');
-      event.preventDefault();
-    });
-  })(jQuery); // End of use strict
+  "use strict";
+// hacer toggle en sidenav
+$("#sidenavToggler").click(function(e) {
+   e.preventDefault();
+    console.log("holi");
+    $("body").toggleClass("sidenav-toggled");
+    $(".sidenav").toggleClass("collapsed");
+    $(".logo").toggleClass("collapsed");
+    $(".material-tooltip").toggleClass("show");
+    
+    $(".sidenav .collapsible-header span").toggleClass("hide");
+    $(".sidenav .collapsible-header i").toggleClass("hide");
+    $(".sidenav .no-collapsible span").toggleClass("hide");
+    $(".sidenav .no-collapsible i").toggleClass("hide");
+    $(".sidenav .collapsible .active .collapsible-body").toggleClass("hide");
+  });
+
+  // Forzar toggled, remover cuando se haga click en collapsed 
+  $(".sidenav .collapsible-header").click(function(e) {
+    e.preventDefault();
+    $("body").removeClass("sidenav-toggled");
+    $(".sidenav").removeClass("collapsed");
+    $(".logo").removeClass("collapsed");
+    $(".sidenav .collapsible-header span").removeClass("hide");
+    $(".sidenav .collapsible-header i").removeClass("hide");
+    $(".sidenav .no-collapsible span").removeClass("hide");
+    $(".sidenav .no-collapsible i").removeClass("hide");
+    $(".sidenav .collapsible .active .collapsible-body").removeClass("hide");
+  });   
+
+  
+})(jQuery); 
